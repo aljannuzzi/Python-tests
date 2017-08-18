65bc94a5b1bc30bfe1c5e168fefe5e5ae9c0fb3b
Alessandro Jannuzzi <alessandro.jannuzzi@microsoft.com>
2017-08-18 11:58:46 -0300

    now with a regex

--- Getfromweb.py
+++ Getfromweb.py
@@ -2,12 +2,20 @@
 
 import requests
 from bs4 import BeautifulSoup
+import re
 
 page = requests.get('http://www.beatlesinterviews.org/index2.html')
-page_soup = BeautifulSoup(page.content, 'html.parser')
 
 if page.status_code == requests.codes.ok:
-    print ('Deu certo')
-    print (page.text)
+  #  print (page.text)
+    page_soup = BeautifulSoup(page.content, 'html.parser')
+
+    #print (page_soup.contents)
+    i=0
+    for item in page_soup.find_all(text=re.compile("^[A-Z]{3,100}")):
+       if i > 1:
+         print (item)
+       i=i+1
+
 else:
     print ('Deu errado')
