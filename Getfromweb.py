08200169d8f1873e24f4f849bff6d1ec918b5350
Alessandro Jannuzzi <alessandro.jannuzzi@microsoft.com>
2017-08-21 10:43:42 -0300

    All songs

--- Getfromweb.py
+++ Getfromweb.py
@@ -4,7 +4,7 @@ import requests
 from bs4 import BeautifulSoup
 import re
 Songs = {}
-
+Songs[1]
 page = requests.get('http://www.beatlesinterviews.org/index2.html')
 
 if page.status_code == requests.codes.ok:
@@ -15,10 +15,21 @@ if page.status_code == requests.codes.ok:
     i=0
     for item in page_soup.find_all(text=re.compile("^[A-Z]{3,100}")):
        if i > 1:
-         for musics in item.next_elements:
-           print (musics)
-         Songs[item]={}
+         print(item)
+         for musics_raw in item.next_elements:
+           musics_raw1 = re.sub(r".*br.*$", "", str(musics_raw))
+           musics_raw2 = re.sub(r".*INCLUD.*", "", str(musics_raw1))
+           musics_raw3 = re.sub(r".*<.*", "", str(musics_raw2))
+           musics_raw4 = re.sub(r"^$", "", str(musics_raw3))
+           musics_raw5 = re.sub(r"\n", "", musics_raw4)
+           musics_raw6 = re.sub(r"Editable", "", musics_raw5)
+          
+           if musics_raw6:
+                 print (musics_raw6)
+                 Songs[item].append(musics_raw6)
        i=i+1
+           
+    print (Songs)   
 
 else:
     print ('Deu errado')
